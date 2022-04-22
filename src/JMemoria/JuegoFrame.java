4ed1ceb4bbb2e622c6cfe3c461f5061b9c286d41
maria1888 <48070915+maria1888@users.noreply.github.com>
2022-04-21 20:05:01 -0600

    clase del frame del juego

--- src/JMemoria/JuegoFrame.java
+++ src/JMemoria/JuegoFrame.java
@@ -1,4 +1,24 @@
 package JMemoria;
 
-public class JuegoFrame {
+import java.awt.Color;
+import java.awt.Toolkit;
+
+import javax.swing.JFrame;
+
+/**
+ * @author -Maria Alvarez
+ * clase JuegoFrame o la ventana del juego donde salen las cartas
+ * @version 2021.3.2
+ */
+
+public class JuegoFrame extends JFrame {
+    public JuegoFrame(){
+        setTitle("Memory Game");
+        setBackground(Color.WHITE);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setBounds(100,200,1200,600);
+        setIconImage(Toolkit.getDefaultToolkit()
+                .getImage(JuegoFrame.class
+                .getResource("/JMemoria/opFondo.png")));
+    }
 }
--- src/JMemoria/MemoriaJuegoApp.java
+++ src/JMemoria/MemoriaJuegoApp.java
@@ -3,7 +3,24 @@ package JMemoria;
 import java.awt.EventQueue;
 import javax.swing.JFrame;
 
+/**
+ * @author Maria Alvarez
+ * Memoria Juego
+ * @version 2021.3.2
+ */
 public class MemoriaJuegoApp extends JFrame {
 
-    public static void main(String[] args)
+    public static void main(String[] args) {
+        EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                try{
+                    JuegoFrame game = new JuegoFrame();
+                    game.setVisible(True);
+                } catch (Exception e){
+                    e.printStackTrace();
+                }
+
+            }
+        });
+    }
 }
